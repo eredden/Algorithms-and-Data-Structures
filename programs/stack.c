19ca447455a9652deed26a93e920a5b3a3d738b3
Evan Redden <redden.evan@gmail.com>
2024-02-06 19:14:04 -0500

    added my implementation of stack

---
+++ programs/stack.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+int peek(stack_t *stack) { return stack -> head -> value; }
+
+void push(stack_t *stack, node_t *node)
+{
+    stack -> length++;
+
+    if (stack -> head == NULL) 
+    { 
+        stack -> head = node;
+        stack -> tail = node; 
+        return;
+    }
+
+    node -> next = stack -> head;
+    stack -> head = node;
+}
+
+int pop(stack_t *stack)
+{
+    if (stack -> length <= 0) { exit(-1); }
+
+    stack -> length--;
+
+    node_t *old_head = stack -> head;
+
+    stack -> head = old_head -> next;
+    old_head -> next = NULL;
+
+    return old_head -> value;
+}
+
+// remove before git push
+int main(void)
+{
+    stack_t stack;
+    node_t first, second, last;
+
+    stack.head = stack.tail = NULL;
+    stack.length = 0;
+
+    first.next = second.next = last.next = NULL;
+    first.value = 1;
+    second.value = 2;
+    last.value = 3;
+
+    push(&stack, &first);
+    push(&stack, &second);
+    push(&stack, &last);
+
+    int value1 = pop(&stack);
+    int value2 = pop(&stack);
+    int value3 = pop(&stack);
+
+    return 0;
+}
---
+++ programs/stack.h
@@ -0,0 +1,23 @@
+#ifndef STACK
+#define STACK
+
+typedef struct node 
+{
+    int value;
+    struct node *next;
+} node_t;
+
+typedef struct stack
+{
+    node_t *head;
+    node_t *tail;
+    int length;
+} stack_t;
+
+int peek(stack_t *stack);
+
+void push(stack_t *stack, node_t *node);
+
+int pop(stack_t *stack);
+
+# endif // STACK
