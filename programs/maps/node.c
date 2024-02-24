703bdb0401e25c2825e319d930b3d844325fadc7
eredden <redden.evan@gmail.com>
2024-02-24 13:52:40 -0500

    removed main to prevent future duplication defintions

--- programs/maps/node.c
+++ programs/maps/node.c
@@ -41,25 +41,3 @@ void node_destructor(node_t* node)
 
     free(node);
 }
-
-int main(void)
-{
-    node_t* node1 = node_factory(1);
-    node_t* node2 = node_factory(2);
-
-    node1 -> next = node2;
-    node2 -> prev = node1;
-
-    node_destructor(node1);
-    node_destructor(node2);
-
-    if (node1 == node2 -> prev || node1 == node2 -> prev)
-    {
-        printf("Destructor function failed to disconnect nodes.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("No errors occurred with the node functions.\n");
-
-    return 0;
-}
