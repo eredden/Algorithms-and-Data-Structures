92ca62c94772f9688f482e03585f440c2924b9e9
eredden <redden.evan@gmail.com>
2024-02-24 21:48:14 -0500

    new hashmap struct, added insert, delete, search functions

--- programs/maps/lru.h
+++ programs/maps/lru.h
@@ -12,17 +12,30 @@ node_t* node_factory(int value);
 void node_destructor(node_t* node);
 
 // map definitions start here
+#define HASHMAP_MAX_SIZE 10
+
 typedef struct map
 {
     int key;
     node_t* value;
+    struct map *next;
 } map_t;
+
+typedef struct hashmap
+{
+    map_t** table;
+} hashmap_t;
+
+hashmap_t* hashmap_factory(void);
+void hashmap_destructor(hashmap_t* hashmap);
 
 map_t* map_factory(int key, node_t* value);
 void map_destructor(map_t* map);
 
-int hash(int key, int size);
-map_t* search(map_t** hash_map, int size, int key);
+int hash(int key);
+void hashmap_insert(hashmap_t* hashmap, int key, node_t* value);
+map_t* hashmap_search(hashmap_t* hashmap, int key);
+void hashmap_remove(hashmap_t* hashmap, int key);
 
 // lru cache defintions start here
 typedef struct lru
--- programs/maps/map.c
+++ programs/maps/map.c
@@ -2,6 +2,57 @@
 #include <stdlib.h>
 #include "lru.h"
 
+hashmap_t* hashmap_factory(void)
+{
+    hashmap_t* hashmap = (hashmap_t*) malloc(sizeof(hashmap_t));
+
+    if (hashmap == NULL)
+    {
+        printf("Memory allocation failed for hashmap.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    hashmap -> table = (map_t**) malloc(HASHMAP_MAX_SIZE * sizeof(map_t));
+
+    if (hashmap -> table == NULL)
+    {
+        printf("Memory allocation failed for hashmap table.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < HASHMAP_MAX_SIZE; i++)
+    {
+        hashmap -> table[i] = NULL;
+    }
+
+    return hashmap;
+}
+
+void hashmap_destructor(hashmap_t* hashmap)
+{
+    if (hashmap == NULL)
+    {
+        printf("Hashmap is a null pointer.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (hashmap -> table == NULL)
+    {
+        printf("Hashmap table is a null pointer.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < HASHMAP_MAX_SIZE; i++)
+    {
+        map_t* map = hashmap -> table[i];
+
+        if (map != NULL) { free(map); }
+    }
+
+    free(hashmap -> table);
+    free(hashmap);
+}
+
 map_t* map_factory(int key, node_t* value)
 {
     map_t* map = (map_t*) malloc(sizeof(map_t));
@@ -14,6 +65,7 @@ map_t* map_factory(int key, node_t* value)
 
     map -> key = key;
     map -> value = value;
+    map -> next = NULL;
 
     return map;
 }
@@ -29,23 +81,93 @@ void map_destructor(map_t* map)
     free(map);
 }
 
-int hash(int key, int size) { return key % size; }
+int hash(int key) { return key % HASHMAP_MAX_SIZE; }
 
-map_t* search(map_t** hash_map, int size, int key)
+void hashmap_insert(hashmap_t* hashmap, int key, node_t* value) 
 {
-    if (hash_map == NULL)
+    if (hashmap == NULL)
     {
-        printf("Hash map is a null pointer.\n");
+        printf("Hashmap is a null pointer.\n");
         exit(EXIT_FAILURE);
     }
 
-    int index = hash(key, size);
-    map_t* map = hash_map[index];
+    if (hashmap -> table == NULL)
+    {
+        printf("Hashmap table is a null pointer.\n");
+        exit(EXIT_FAILURE);
+    }
 
-    while (map -> key != 0)
+    int index = hash(key);
+    map_t* map = map_factory(key, value);
+
+    map -> next = hashmap -> table[index]; // may need adjustment
+    hashmap -> table[index] = map;
+}
+
+map_t* hashmap_search(hashmap_t* hashmap, int key)
+{
+    if (hashmap == NULL)
+    {
+        printf("Hashmap is a null pointer.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (hashmap -> table == NULL)
+    {
+        printf("Hashmap table is a null pointer.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int index = hash(key);
+    map_t* map = hashmap -> table[index];
+
+    while (map != NULL)
     {
         if (map -> key == key) { return map; }
-        if (map -> key != key && index < size) { index++; }
-        else { break; }
+        if (map -> key != key && map -> next != NULL) { map = map -> next; }
+        else { return NULL; }
     }
+
+    return NULL;
+}
+
+void hashmap_remove(hashmap_t* hashmap, int key)
+{
+    if (hashmap == NULL)
+    {
+        printf("Hashmap is a null pointer.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (hashmap -> table == NULL)
+    {
+        printf("Hashmap table is a null pointer.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int index = hash(key);
+    map_t* map = hashmap -> table[index];
+
+    while (map != NULL)
+    {
+        map_t* next_map = map -> next;
+
+        if (map -> key == key) 
+        { 
+            hashmap -> table[index] = next_map; 
+            free(map);
+            return;
+        }
+
+        if (next_map -> key == key) 
+        {
+            map -> next = next_map -> next;
+            free(next_map);
+            return;
+        }
+
+        map = next_map;
+    }
+
+    return;
 }
